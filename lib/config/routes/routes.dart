adf80ad8dfba96deef8a27df3cecfa0663102993
Mochammad Ramadhany <18220087@std.stei.itb.ac.id>
2023-08-29 13:28:06 +0700

    feat : migrate bottom navigation bar into using go_router

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mytradeasia/view/auth/biodata/biodata_screen.dart';
 import 'package:mytradeasia/view/auth/choose_role/role_user_screen.dart';
@@ -5,8 +6,15 @@ import 'package:mytradeasia/view/auth/login/forgot_password/forgot_password_scre
 import 'package:mytradeasia/view/auth/login/forgot_password/reset_password_screen.dart';
 import 'package:mytradeasia/view/auth/login/login_screen.dart';
 import 'package:mytradeasia/view/auth/register/register_screen.dart';
+import 'package:mytradeasia/view/menu/history/history_screen.dart';
+import 'package:mytradeasia/view/menu/home/home_screen.dart';
+import 'package:mytradeasia/view/menu/messages/messages_screen.dart';
+import 'package:mytradeasia/view/menu/mytradeasia/mytradeasia_screen.dart';
 import 'package:mytradeasia/view/menu/other/navigation_bar.dart';
 import 'package:mytradeasia/view/menu/other/splash_page.dart';
+
+final GlobalKey<NavigatorState> _shellNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 class Routes {
   GoRouter router =
@@ -15,9 +23,33 @@ class Routes {
         path: "/",
         builder: (context, state) => const SplashScreen(),
         redirect: (context, state) => '/'),
-    GoRoute(
-        path: "/home",
-        builder: (context, state) => const NavigationBarWidget()),
+    // GoRoute(
+    //     path: "/home",
+    //     builder: (context, state) => const NavigationBarWidget()),
+    ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (BuildContext context, GoRouterState state, Widget child) =>
+            NavigationBarWidget(
+              child: child,
+            ),
+        routes: [
+          GoRoute(
+              path: "/home",
+              pageBuilder: (context, state) => NoTransitionPage(
+                  child: const HomeScreen(), key: state.pageKey)),
+          GoRoute(
+              path: "/message",
+              pageBuilder: (context, state) => NoTransitionPage(
+                  child: const MessageScreen(), key: state.pageKey)),
+          GoRoute(
+              path: "/history",
+              pageBuilder: (context, state) => NoTransitionPage(
+                  child: const HistoryScreen(), key: state.pageKey)),
+          GoRoute(
+              path: "/mytradeasia",
+              pageBuilder: (context, state) => NoTransitionPage(
+                  child: const MyTradeAsiaScreen(), key: state.pageKey))
+        ]),
     GoRoute(
         path: "/auth",
         name: "choose_role",
