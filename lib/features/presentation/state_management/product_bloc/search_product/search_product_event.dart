02ede08e4ece4cbc4dbbf62efbdfbd79121ea568
Mochammad Ramadhany <18220087@std.stei.itb.ac.id>
2023-09-08 10:06:06 +0700

    feat : migrate to bloc in the search_product_screen.dart

--- lib/features/presentation/state_management/product_bloc/search_product/search_product_event.dart
+++ lib/features/presentation/state_management/product_bloc/search_product/search_product_event.dart
@@ -6,3 +6,7 @@ class SearchProduct extends SearchProductEvent {
   final String query;
   const SearchProduct(this.query);
 }
+
+class ClearSearch extends SearchProductEvent {
+  const ClearSearch();
+}
